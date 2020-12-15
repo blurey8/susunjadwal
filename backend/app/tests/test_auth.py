9decbb22486800ceee83ae1a5b35ce8df6fd2328
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-12-15 23:48:12 +0700

    Move client() fixture to root conftest.py

--- backend/app/tests/test_auth.py
+++ backend/app/tests/test_auth.py
@@ -1,10 +1,9 @@
 import pytest
 from app import app
 from app.tests.utils import SERVICE_URL, get_ticket_from_sso_ui
-from utils.fixtures import mongo
-import mongoengine as me
 
-BASE_PATH = app.config["BASE_PATH"] 
+BASE_PATH = app.config["BASE_PATH"]
+
 
 @pytest.mark.usefixtures('mongo')
 class TestAuth:
@@ -16,11 +15,10 @@ class TestAuth:
         assert ticket is not None
 
         res = client.post(BASE_PATH + '/auth/',
-                        json={'ticket': ticket, 'service_url': service_url})
+                          json={'ticket': ticket, 'service_url': service_url})
         assert res.status_code == 200
 
         res_data = res.get_json()
         assert res_data['token'] is not None
         assert res_data['major_id'] is not None
         assert res_data['user_id'] is not None
-        
--- backend/conftest.py
+++ backend/conftest.py
@@ -1,5 +1,22 @@
 import pytest
+from flask import current_app
 from mongoengine import disconnect, connect
+
+from app import app
+
+
+@pytest.fixture(scope='module')
+def client():
+    """Generate test client for every module"""
+    with app.test_client() as client:
+        with app.app_context():
+            app.config.update(
+                TESTING=True,
+                ENV='test'
+            )
+            assert current_app.config["TESTING"]
+            assert current_app.config["ENV"] == "test"
+        yield client
 
 
 @pytest.fixture(scope="function")
