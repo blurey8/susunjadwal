f0d155695c4c2ab307dbaea0938a42cd16516bf4
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-12-27 15:14:21 +0700

    Add negative tests for /auth/ path

--- backend/app/tests/test_auth.py
+++ backend/app/tests/test_auth.py
@@ -1,25 +1,49 @@
 import pytest
-from .utils import (BASE_PATH, SERVICE_URL,
-                    get_ticket_from_sso_ui, is_credentials_available)
+from .utils import (
+    BASE_PATH,
+    SERVICE_URL,
+    get_ticket_from_sso_ui,
+    is_credentials_available,
+)
 
 
 @pytest.mark.sso
-@pytest.mark.skipif(condition=not is_credentials_available(),
-                    reason='SSO credentials is not available')
-@pytest.mark.usefixtures('mongo')
+@pytest.mark.skipif(
+    condition=not is_credentials_available(), reason="SSO credentials is not available"
+)
+@pytest.mark.usefixtures("mongo")
 class TestAuth:
     """Test authentication endpoint"""
 
-    def test_auth(self, client):
+    @classmethod
+    def setup_class(cls):
+        cls.path = BASE_PATH + "/auth/"
+
+    def test_request_with_valid_auth_should_return_user_data(self, client):
         ticket = get_ticket_from_sso_ui()
         service_url = SERVICE_URL
         assert ticket is not None
 
-        res = client.post(BASE_PATH + '/auth/',
-                          json={'ticket': ticket, 'service_url': service_url})
-        assert res.status_code == 200
+        response = client.post(
+            self.path, json={"ticket": ticket, "service_url": service_url}
+        )
+        assert response.status_code == 200
 
-        res_data = res.get_json()
-        assert res_data['token'] is not None
-        assert res_data['major_id'] is not None
-        assert res_data['user_id'] is not None
+        res_data = response.get_json()
+        assert res_data["token"] is not None
+        assert res_data["major_id"] is not None
+        assert res_data["user_id"] is not None
+
+    def test_request_with_invalid_auth_should_return_400(self, client):
+        request_body = {"tiket": "invalid_ticket", "service_url": "invalid_service_url"}
+
+        response = client.post(self.path, json=request_body)
+
+        assert response.status_code == 400
+        assert response.get_json() == {}
+
+    def test_request_without_request_body_should_return_400(self, client):
+        response = client.post(self.path, json={})
+
+        assert response.status_code == 400
+        assert response.get_json() == {}
