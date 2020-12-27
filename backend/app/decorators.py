6b0b348d6738791384d490161ca69e57d4712ad8
Farhan Azyumardhi Azmi <sonofkyrat99@gmail.com>
2020-12-27 12:07:29 +0700

    Reformat app/decorators.py

--- backend/app/decorators.py
+++ backend/app/decorators.py
@@ -8,11 +8,17 @@ def require_same_user_id(func):
     @functools.wraps(func)
     def decorated_func(*args, **kwargs):
         data = extract_header_data(request.headers)
-        if(data['user_id'] == kwargs['user_id']):
+        if data["user_id"] == kwargs["user_id"]:
             return func(*args, **kwargs)
-        return jsonify({
-            'message': 'Unauthorized. Only the resource owner can access this endpoint'
-        }), 401
+        return (
+            jsonify(
+                {
+                    "message": "Unauthorized. Only the resource owner can access this endpoint"
+                }
+            ),
+            401,
+        )
+
     return decorated_func
 
 
@@ -21,8 +27,7 @@ def require_jwt_token(func):
     def decorated_func(*args, **kwargs):
         data = extract_header_data(request.headers)
         if data is None:
-            return (jsonify({
-                'message': 'There is no token/token is not valid'
-            }), 401)
+            return jsonify({"message": "There is no token/token is not valid"}), 401
         return func(*args, **kwargs)
+
     return decorated_func
